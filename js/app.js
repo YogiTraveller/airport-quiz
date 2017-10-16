185d16839971baf8818c858fd9df11c6a9c7d426
Maciej Pienczewski <maciej.pienczewski@gmail.com>
2017-10-16 16:30:53 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+
+var allQuestions = [
+  {question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0},
+  {question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0},
+  {question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0}
+];
+
+
+var quiz = {
+
+    settings: {
+
+    },
+
+
+    
+
+
+}
